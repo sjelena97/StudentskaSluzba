f7542f5b29c27ca777fbd03baed50bbe426b4c1e
Jelena <lelaug97@hotmail.com>
2021-03-02 01:16:45 +0100

    Prvi commit, dodan projekat na repozitorij

---
+++ projekat-eo/src/main/java/com/spring/projekateo/ProjekatEoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.projekateo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatEoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatEoApplication.class, args);
+	}
+
+}
---
+++ projekat-eo/src/test/java/com/spring/projekateo/ProjekatEoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.projekateo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjekatEoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
