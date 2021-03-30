b33486c4935a919e97e621e40c844ff5f6c755f3
danijel98 <danijel_vuk@hotmail.com>
2021-03-30 06:01:21 +0200

    Correction for updateCourse

--- projekat-eo/src/main/java/com/spring/projekateo/controller/CourseController.java
+++ projekat-eo/src/main/java/com/spring/projekateo/controller/CourseController.java
@@ -25,59 +25,61 @@ public class CourseController {
 
 	@Autowired
 	private CourseService courseService;
-	
-	
-	@GetMapping("/getAllCourses") 
+
+	@GetMapping("/getAllCourses")
 	public ResponseEntity<List<CourseDTO>> getAllCourses() {
 		List<Course> courses = courseService.getAllCourses();
-		//convert courses to DTOs
+		// convert courses to DTOs
 		List<CourseDTO> coursesDTO = new ArrayList<>();
 		for (Course c : courses) {
 			coursesDTO.add(new CourseDTO(c));
 		}
 		return new ResponseEntity<>(coursesDTO, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/addCourse")
 	public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO courseDTO) {
-		 
+
 		Course course = courseService.findCourseByCode(courseDTO.getCode());
-			if (course != null) {
+		if (course != null) {
 			return new ResponseEntity<CourseDTO>(HttpStatus.BAD_REQUEST);
 		}
-		
+
 		Course c = new Course();
-		
+
 		c.setCode(courseDTO.getCode());
 		c.setName(courseDTO.getName());
 		c.setESPB(courseDTO.getESPB());
-		
+
 		c = this.courseService.save(c);
-				
-		return new ResponseEntity<CourseDTO>(new CourseDTO(c), HttpStatus.CREATED);	
+
+		return new ResponseEntity<CourseDTO>(new CourseDTO(c), HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/updateCourse/{course_id}")
-	public ResponseEntity<CourseDTO> updateCourse(@RequestBody CourseDTO courseDTO, @PathVariable("course_id") int course_id){
+	public ResponseEntity<CourseDTO> updateCourse(@RequestBody CourseDTO courseDTO,
+			@PathVariable("course_id") int course_id) {
 
 		Course course = courseService.findById(courseDTO.getId());
 		if (course == null) {
 			return new ResponseEntity<CourseDTO>(HttpStatus.BAD_REQUEST);
 		}
-		
-		Course course1 = courseService.findCourseByCode(courseDTO.getCode());
-		if (course1 != null) {
-			return new ResponseEntity<CourseDTO>(HttpStatus.BAD_REQUEST);
+
+		if (!courseDTO.getCode().equalsIgnoreCase(course.getCode())) {
+			Course course1 = courseService.findCourseByCode(courseDTO.getCode());
+
+			if (course1 != null) {
+				return new ResponseEntity<CourseDTO>(HttpStatus.BAD_REQUEST);
+			}
 		}
-		
+
 		course.setCode(courseDTO.getCode());
 		course.setName(courseDTO.getName());
 		course.setESPB(courseDTO.getESPB());
-		
+
 		course = this.courseService.save(course);
-		
-		return new ResponseEntity<CourseDTO>(new CourseDTO(course), HttpStatus.OK);	
+
+		return new ResponseEntity<CourseDTO>(new CourseDTO(course), HttpStatus.OK);
 	}
-	
-	
+
 }
