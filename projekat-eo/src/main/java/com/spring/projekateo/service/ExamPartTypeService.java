d210672445a6e4c09f74a2140d9a7e2cf9347564
Jelena <lelaug97@hotmail.com>
2021-07-13 17:29:35 +0200

    add update exam parts(admin)

--- projekat-eo/src/main/java/com/spring/projekateo/service/ExamPartTypeService.java
+++ projekat-eo/src/main/java/com/spring/projekateo/service/ExamPartTypeService.java
@@ -1,5 +1,7 @@
 package com.spring.projekateo.service;
 
+import java.util.List;
+
 import com.spring.projekateo.model.ExamPartType;
 
 public interface ExamPartTypeService {
@@ -7,5 +9,7 @@ public interface ExamPartTypeService {
 	ExamPartType findById(Integer id);
 
 	ExamPartType findByCode(String code);
+
+	List<ExamPartType> getAllExamPartTypes();
 
 }
