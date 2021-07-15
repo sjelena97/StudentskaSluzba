38f1a5d9746dbe9982db009b526b8b14f36cdef0
Jelena <lelaug97@hotmail.com>
2021-07-15 23:42:37 +0200

    upcoming exams(home page)

--- projekat-eo-angular/angularApp/src/app/components/sluzba-home/sluzba-home.component.ts
+++ projekat-eo-angular/angularApp/src/app/components/sluzba-home/sluzba-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ExamPart } from 'src/app/model/examPart';
+import { AuthenticationServiceService } from 'src/app/services/auth/authentication-service.service';
+import { SluzbaExamsServiceService } from '../sluzba-exams/sluzba-exams-service.service';
 
 @Component({
   selector: 'app-sluzba-home',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SluzbaHomeComponent implements OnInit {
 
-  constructor() { }
+  examParts: ExamPart[];
+
+  constructor(private examPartsService: SluzbaExamsServiceService, private authService: AuthenticationServiceService) {
+  }
 
   ngOnInit(): void {
+    this.examPartsService.getComingExamPartsForUser().subscribe(res =>
+      this.examParts = res.body);
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 
 }
